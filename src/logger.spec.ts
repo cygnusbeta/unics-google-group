b0ead9a3e7bfd7001e45e98a39464c934d5f07ef
cygnusbeta <betacygnib+git@gmail.com>
2019-09-17 17:12:17 +0900

    Fix 4511da373db7406625cf34cfa88152a02795122e

--- src/logger.spec.ts
+++ src/logger.spec.ts
@@ -2,7 +2,7 @@ import { logVar } from './logger';
 jest.unmock('./logger');
 
 describe('logger', () => {
-  it('logVar(varName)', () => {
+  it('logVar({ varName })', () => {
     const varName = 'value';
     expect(logVar({ varName })).toBe('varName: value');
   });
